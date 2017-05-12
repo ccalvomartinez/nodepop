8d383ed27ae2a24a96cba66bfbea65a7e2b4b340
Carolina Calvo <pececillu@gmail.com>
2017-05-12 14:04:47 +0200

    Defino los esquemas de Ads y Users

--- models/Ad.js
+++ models/Ad.js
@@ -1,17 +1,30 @@
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 // primero definimos un esquema
-const addSchema =
-    // @ts-ignore
-    mongoose.Schema({
-        name: String,
-        price: Number,
-       // ....
+const adSchema = new Schema({
+    name: {
+        type:String,
+        required:true
+    },
+    category: {
+        type:String,
+        enum:['vende','busca']    
+    },
+    price: {
+        type:Number,
+        min:1
+    },
+    picture: String,
+    tags:[{
+        type:String,
+        enum:['work','lifeStyle','motor','mobile']    
+    }]
     });
 
 // Luego creamos el modelo
 
-const Ad = mongoose.model('Ad', addSchema);
+const Ad = mongoose.model('Ad', adSchema);
+
 //Realmente no es necesario exportarlo porque  podríamos recuperarlo usando
-//mongoose.models('Agente)
+//mongoose.models('Ad)
 module.exports = Ad;
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// primero definimos un esquema
+const userSchema = new Schema({
+        name: {
+            type: String,
+            required:true    
+    },
+        email: {
+            type:String,
+            required: true,
+            match:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/    
+    },
+        password: {
+            type:String,
+            required: true,
+            minlength:6   
+    }
+    });
+
+// Luego creamos el modelo
+
+const User = mongoose.model('User', userSchema);
+
+//Realmente no es necesario exportarlo porque  podríamos recuperarlo usando
+//mongoose.models('Ad)
+module.exports = User;
