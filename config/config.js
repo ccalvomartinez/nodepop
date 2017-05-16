76b37a7da643fa84712293af04111095da4a80f9
Carolina Calvo <pececillu@gmail.com>
2017-05-16 14:17:00 +0200

    Corrección de fichero configuración. saltRounds

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports.loadDataToDb = true;
+module.exports.saltRounds = 10;
 module.exports.jwt = {
     secret: 'BgV7G@fqJS2xR*HJ',
-    expiresInSeconds: 24 * 60 * 60,
-    saltRounds: 10
+    expiresInSeconds: 24 * 60 * 60
 };
